881d29cd0855d9fdbf5c81de18231c2bc1797531
anthobarbarich <anthony@mnrl.co>
2019-10-18 19:31:05 +0200

    test v3

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 const gulpNewer = require("gulp-newer");
-const changed = require('gulp-changed');
 const htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
@@ -16,37 +15,7 @@ gulp.task('minify', () => {
 
 gulp.task('images', () =>
     gulp.src(['static/uploads/*', '!static/uploads/*.svg'])
-        .pipe(gulpNewer('public/thumbs/images/uploads'))
-        .pipe(imagemin([    
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.jpegtran({progressive: true}),
-            imageminMozjpeg({
-                quality: 60
-            }),
-            imagemin.optipng({optimizationLevel: 7}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
-        .pipe(gulpImageresize({
-            width : 600,
-			quality : 0.6,
-          }))
-        .pipe(gulp.dest('static/compressed/images/uploads'))
-        .pipe(gulpImageresize({
-            width : 480,
-			quality : 0.7,
-          }))
-        .pipe(gulp.dest('static/thumbs/images/uploads')) 
-	
-);
-
-gulp.task('images', () =>
-    gulp.src(['static/uploads/*', '!static/uploads/*.svg'])
-        .pipe(gulpNewer('public/thumbs/images/uploads'))
+        .pipe(gulpNewer('static/thumbs/images/uploads'))
         .pipe(imagemin([    
             imagemin.gifsicle({interlaced: true}),
             imagemin.jpegtran({progressive: true}),
